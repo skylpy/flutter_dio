e111af757f0f2ea36f88ae2ce9ce5d218456cc72
sky <807446126@qq.com>
2020-05-12 11:13:53 +0800

    commit

---
+++ Flutter知识点-HTTP请求-源码/flutter_node_server/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.resolve(__dirname,"public")));
+
+app.use(function(req,res,next){
+    const proxy = req.query.proxy;
+    if(proxy){
+        req.header.cookie = req.header.cookie + `__proxy__${proxy}`;
+    }
+    next();
+});
+
+app.use("/getHttpData",require("./router/httpData"))
+app.use("/getHttpClientData",require("./router/httpClientData"))
+app.use("/getDioData",require("./router/dioData"))
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`server running @http://localhost:${port}`);
+});
+
+module.exports = app;
